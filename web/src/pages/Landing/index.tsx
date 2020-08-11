7b56ce3ae28117567b675f9670b635e82ad6d0a8
Guilherme Sant'Ana <guilherme.sant_ana@outlook.com>
2020-08-11 18:46:26 -0300

    add new landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../services/api'
 
-
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
+import logoutIcon from '../../assets/images/icons/logout-icon.svg';
+
 
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
@@ -14,9 +15,9 @@ import './styles.css';
 
 
 function Landing() {
-  const [ totalConnections, setTotalConnections ] = useState(0)
-  
-  useEffect(()=> {
+  const [totalConnections, setTotalConnections] = useState(0)
+
+  useEffect(() => {
     api.get('connections').then(response => {
       const { total } = response.data
       setTotalConnections(total)
@@ -27,32 +28,49 @@ function Landing() {
     <div id="page-landing">
       <div id="page-landing-content" className="container">
 
-        <div className="logo-container-landing">
-          <img src={logoImg} alt="Proffy logo"/>
-          <h2>Sua plataforma de estudos online.</h2>
+        <div id="top">
+          <div id="user-header">
+            <div id="user-info">
+              <img src="http://github.com/GDSRS.png" alt="foto do usuário" />
+              <p>User name</p>
+            </div>
+            <img src={logoutIcon} alt="botão sair" />
+          </div>
+
+          <div className="logo-container-landing">
+            <img src={logoImg} alt="Proffy logo" />
+            <h2>Sua plataforma de<br/> estudos online.</h2>
+          </div>
+
+          <img
+            src={landingImg}
+            alt="Plataforma de estudos"
+            className="hero-image"
+          />
         </div>
 
-        <img 
-          src={landingImg}
-          alt="Plataforma de estudos"
-          className="hero-image"
-        />
+        <div id="bottom">
+          <p id="text-presentation">
+            Seja bem-vindo<br />
+            <strong>O que deseja fazer ?</strong>
+          </p>
+          <div className="buttons-container">
+            <Link to="/study" className="study">
+              <img src={studyIcon} alt="Estudar" />
+              Estudar
+            </Link>
 
-        <div className="buttons-container">
-          <Link to="/study" className="study">
-            <img src={studyIcon} alt="Estudar"/>
-            Estudar
-          </Link>
-
-          <Link to="/give-classes" className="give-classes">
-            <img src={giveClassesIcon } alt="Lecionar"/>
+            <Link to="/give-classes" className="give-classes">
+              <img src={giveClassesIcon} alt="Lecionar" />
             Lecionar
           </Link>
-        </div>
+          </div>
 
-        <span className="total-connections">
-          Total de {totalConnections} conexões ja realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
-        </span>
+          <span className="total-connections">
+            Total de {totalConnections} conexões<br/>
+            já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+          </span>
+        </div>
       </div>
     </div>
   );
